0fee8214edd3c7da063464d7138db93ee97317b7
SaketCodeScribe <saket0565@gmail.com>
2026-08-24 00:50:43 +0530

    Add Splitwise

--- src/Problems/FinancialAndPaymentSystems/Splitwise.java
+++ src/Problems/FinancialAndPaymentSystems/Splitwise.java
@@ -36,18 +36,103 @@ public class Splitwise {
     }
 
     interface SplittingStrategy {
-        Map<String, Map<String, Double>> split(Map<String, Map<String, Double>> owe, Record record);
+        Map<User, Map<User, Double>> split(Map<User, Map<User, Double>> owe, Record record);
     }
 
     static class ExpenseSplitter implements SplittingStrategy {
+        public synchronized Map<User, Map<User, Double>> split(Map<User, Map<User, Double>> owe, Record record) {
+            updateOwe(owe, record);
+            List<Map.Entry<User, Double>> amount = traverseOwe(owe);
+            amount.sort(Comparator.comparingDouble(Map.Entry::getValue));
+            return split(amount);
+        }
 
+        private Map<User, Map<User, Double>> split(List<Map.Entry<User, Double>> amount) {
+            int i = 0, j = amount.size() - 1;
+            Map<User, Map<User, Double>> owe = new HashMap<>();
+
+            while (i < j) {
+                Map.Entry<User, Double> entry1 = amount.get(i);
+                Map.Entry<User, Double> entry2 = amount.get(j);
+                if (entry1.getValue() == 0) {
+                    i++;
+                    continue;
+                }
+                if (entry2.getValue() == 0) {
+                    j--;
+                    continue;
+                }
+                User usrA = entry1.getKey();
+                double amtA = -entry1.getValue();
+                User usrB = entry2.getKey();
+                double amtB = entry2.getValue();
+                if (amtA <= amtB) {
+                    owe.compute(usrA, (key, value) -> {
+                        if (value == null) {
+                            value = new HashMap<>();
+                        }
+                        value.compute(usrB, (k, v) -> v == null ? amtA : v + amtA);
+                        return value;
+                    });
+                    entry2.setValue(entry2.getValue() - amtA);
+                    i++;
+                    if (amtA == amtB) j--;
+                } else if (amtA > amtB) {
+                    owe.compute(usrA, (key, value) -> {
+                        if (value == null) {
+                            value = new HashMap<>();
+                        }
+                        value.compute(usrB, (k, v) -> v == null ? amtB : v + amtA);
+                        return value;
+                    });
+                    entry1.setValue(entry1.getValue() - amtB);
+                    j--;
+                }
+            }
+            return owe;
+        }
+
+        private List<Map.Entry<User, Double>> traverseOwe(Map<User, Map<User, Double>> owe) {
+            Map<User, Double> amount = new HashMap<>();
+            for (Map.Entry<User, Map<User, Double>> entry : owe.entrySet()) {
+                User payer = entry.getKey();
+                double amt = 0;
+                for (Map.Entry<User, Double> mapEntry : entry.getValue().entrySet()) {
+                    amt -= amount.compute(mapEntry.getKey(), (k, v) -> {
+                        if (v == null) v = 0d;
+                        return v + mapEntry.getValue();
+                    });
+                }
+                amount.put(payer, amount.getOrDefault(payer, 0d) + amt);
+            }
+            return amount.entrySet().stream().toList();
+        }
+
+        private void updateOwe(Map<User, Map<User, Double>> owe, Record record) {
+            User user = record.user();
+            double amt = record.amount();
+            for (User usr : record.splitBetween()) {
+                owe.compute(usr, (key, value) -> {
+                    if (value == null) {
+                        value = new HashMap<>();
+                    }
+                    value.compute(user, (k, v) -> {
+                        if (v == null) v = 0d;
+                        v += amt;
+                        return v;
+                    });
+                    return value;
+                });
+            }
+        }
     }
 
 
     record User(String name) {
     }
 
-    record Record(String description, Double amount, User user, String<User> splitBetween, Transaction transaction, ZonedDateTime creationTime) {
+    record Record(String description, Double amount, User user, Set<User> splitBetween, Transaction transaction,
+                  ZonedDateTime creationTime) {
     }
 
     static class Notification implements Observer {
